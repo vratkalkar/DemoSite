56d5399222006badff59851bd6e2bb74c0bad79e
Vivek <vratkalkar@live.com>
2014-03-23 20:51:43 -0400

    updated to sidebar navigation

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ $(document).ready(function() {
   var options = {miniMargin: 5, maxMargin: 15, itemSelector: ".item", firstItemClass: "first-item"};
   $(".container").rowGrid(options);
 });
+
+
+
+
+
