57009c6f69abb4312611a0cb72bb4917d207032a
Vivek <vratkalkar@live.com>
2014-04-12 11:55:16 -0400

    added Twitter share button

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ $(document).ready(function() {
  });
 });
 
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
+
+
+$(document).ready(function() {
+  var options = {minMargin: 0, maxMargin: 15, itemSelector: ".item", firstItemClass: "first-item"};
+  $(".container").rowGrid(options);
+});
